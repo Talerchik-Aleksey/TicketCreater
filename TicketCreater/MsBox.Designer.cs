68847ce58064587b45366a1fbe51e86e77dc8f2b
Talerchik Alexey <talerchik@protonmail.com>
2022-08-09 16:25:54 +0300

    авп

---
+++ TicketCreater/MsBox.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace TicketCreater
+{
+    partial class MsBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Caption = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.BodyLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.Caption);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(512, 35);
+            this.panel1.TabIndex = 11;
+            // 
+            // Caption
+            // 
+            this.Caption.AutoSize = true;
+            this.Caption.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Caption.Location = new System.Drawing.Point(5, 7);
+            this.Caption.Margin = new System.Windows.Forms.Padding(10, 0, 3, 0);
+            this.Caption.Name = "Caption";
+            this.Caption.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.Caption.Size = new System.Drawing.Size(67, 19);
+            this.Caption.TabIndex = 7;
+            this.Caption.Text = "Caption";
+            this.Caption.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pictureBox1.Image = global::TicketCreater.Properties.Resources.TitleBarClose;
+            this.pictureBox1.Location = new System.Drawing.Point(469, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(43, 35);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::TicketCreater.Properties.Resources.ButtonNext;
+            this.pictureBox5.Location = new System.Drawing.Point(300, 180);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(180, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox5.TabIndex = 14;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            this.pictureBox5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox5_MouseDown);
+            this.pictureBox5.MouseEnter += new System.EventHandler(this.pictureBox5_MouseEnter);
+            this.pictureBox5.MouseLeave += new System.EventHandler(this.pictureBox5_MouseLeave);
+            this.pictureBox5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox5_MouseUp);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::TicketCreater.Properties.Resources.Button_2;
+            this.pictureBox4.Location = new System.Drawing.Point(21, 180);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(180, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            this.pictureBox4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox5_MouseDown);
+            this.pictureBox4.MouseEnter += new System.EventHandler(this.pictureBox5_MouseEnter);
+            this.pictureBox4.MouseLeave += new System.EventHandler(this.pictureBox5_MouseLeave);
+
+            this.pictureBox4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox5_MouseUp);
+            // 
+            // BodyLabel
+            // 
+            this.BodyLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BodyLabel.Location = new System.Drawing.Point(5, 59);
+            this.BodyLabel.Margin = new System.Windows.Forms.Padding(10, 0, 3, 0);
+            this.BodyLabel.Name = "BodyLabel";
+            this.BodyLabel.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.BodyLabel.Size = new System.Drawing.Size(495, 89);
+            this.BodyLabel.TabIndex = 12;
+            this.BodyLabel.Text = "Title";
+            // 
+            // MsBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(512, 262);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.BodyLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MsBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MsBox";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label Caption;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox4;
+        private Label BodyLabel;
+    }
+}
---
+++ TicketCreater/MsBox.cs
@@ -0,0 +1,68 @@
+﻿using TicketCreater.Properties;
+
+namespace TicketCreater
+{
+    public partial class MsBox : Form
+    {
+        public MsBox(string caption, string text)
+        {
+            InitializeComponent();
+            Caption.Text = caption;
+            BodyLabel.Text = text;
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void pictureBox5_MouseDown(object sender, MouseEventArgs e)
+        {
+            var pictureBox = (PictureBox)sender;
+            if (sender.Equals(pictureBox5))
+            {
+                pictureBox.Image = Resources.PNextButton;
+            }
+            else if (sender.Equals(pictureBox4))
+            {
+                pictureBox.Image = Resources.PButtonClose;
+            }
+        }
+
+        private void pictureBox5_MouseEnter(object sender, EventArgs e)
+        {
+            var pictureBox = (PictureBox)sender;
+            if (sender.Equals(pictureBox5))
+            {
+                pictureBox.Image = Resources.HButtonNext;
+            }
+            else if (sender.Equals(pictureBox4))
+            {
+                pictureBox.Image = Resources.HButtonClose;
+            }
+        }
+
+        private void pictureBox5_MouseUp(object sender, MouseEventArgs e)
+        {
+            pictureBox5.Image = Resources.ButtonNext;
+            pictureBox4.Image = Resources.ButtonClose;
+
+        }
+
+        private void pictureBox5_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox5.Image = Resources.ButtonNext;
+            pictureBox4.Image = Resources.ButtonClose;
+        }
+    }
+}
